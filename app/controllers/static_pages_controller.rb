80c355011cd6c7b693ce5724baa5472080ab7ede
Kemal Mutlu <mutlukemal1717@gmail.com>
2022-04-15 02:09:58 +0300

    styling courses and home/index

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
+  skip_before_action :authenticate_user!, :only => [:index]
+
   def landing_pages
+    @courses = Course.all.limit(3)
+    @latest_couses = Course.all.limit(3).order(created_at: :desc)
   end
 
   def privacy_policy
