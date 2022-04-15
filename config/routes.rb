4b99f48d47e24e2c658ca1387ca5a5bf744fa977
Kemal Mutlu <mutlukemal1717@gmail.com>
2022-04-16 01:08:10 +0300

    devise trackable, users/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :courses
+  resources :users, only: [:index]
   root "static_pages#landing_pages"
   get "privacy-policy", to: "static_pages#privacy_policy"
 end
