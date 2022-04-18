308642d67f8f211321f00f3dc7497ebfe0d6be9c
Kemal Mutlu <mutlukemal1717@gmail.com>
2022-04-19 01:51:57 +0300

    gem pundit - only admin can edit other users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  protect_from_forgery
 
   include Pundit
-  protect_from_forgery
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   include PublicActivity::StoreController #save current_user using gem public_activity
 
   before_action :set_global_variables, if: :user_signed_in?
-
   def set_global_variables
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search) #navbar search
   end
@@ -19,4 +18,5 @@ class ApplicationController < ActionController::Base
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(request.referrer || root_path)
   end
+
 end
